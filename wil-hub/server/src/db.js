9f4afb75c6b3ef2251942886726005c395ae05d6
JR Escert <uhoneprincemulaudzi@gmail.com>
2026-08-19 11:20:30 +0200

    modified db.js

--- wil-hub/server/src/db.js
+++ wil-hub/server/src/db.js
@@ -2,11 +2,8 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.PGHOST || 'localhost',
-  port: process.env.PGPORT || 5432,
-  database: process.env.PGDATABASE || 'wil_hub_db',
-  user: process.env.PGUSER || 'postgres',
-  password: process.env.PGPASSWORD || '',
+  connectionString: process.env.DATABSE_URL,
+  ssl: {rejectUnauthorized: false}
 });
 
 pool.on('error', (err) => {
